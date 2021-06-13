11cdf97b6379e061ca8538f97385d89334b8e3c8
Lukasz K <kanicki.lukasz@gmail.com>
2021-06-13 19:20:50 +0200

    initial commit

---
+++ backend/src/main/java/pl/seahawks/vacationes/VacationesApplication.java
@@ -0,0 +1,13 @@
+package pl.seahawks.vacationes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationesApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/pl/seahawks/vacationes/VacationesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.seahawks.vacationes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
