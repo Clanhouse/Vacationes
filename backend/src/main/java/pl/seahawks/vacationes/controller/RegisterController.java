0cae78ccb463abc554419f372dda9f638d6e7931
z.skomra <z.skomraa@gmail.com>
2021-08-24 15:17:37 +0200

    VAC-21 :: add registration responses

--- backend/src/main/java/pl/seahawks/vacationes/controller/RegisterController.java
+++ backend/src/main/java/pl/seahawks/vacationes/controller/RegisterController.java
@@ -2,15 +2,16 @@ package pl.seahawks.vacationes.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.seahawks.vacationes.request.RegisterRequest;
+import pl.seahawks.vacationes.response.RegisterResponse;
 import pl.seahawks.vacationes.service.RegistrationService;
+
 
 @Controller
 @RestController
@@ -22,8 +23,9 @@ public class RegisterController {
     private final RegistrationService registrationService;
 
     @PostMapping
-    public Long registerUser(@RequestBody RegisterRequest request) {
-        return registrationService.registerNewUser(request);
+    public ResponseEntity<RegisterResponse> regUser(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok()
+                .body(registrationService.registerNewUser(request));
     }
 
 
--- backend/src/main/java/pl/seahawks/vacationes/service/RegistrationService.java
+++ backend/src/main/java/pl/seahawks/vacationes/service/RegistrationService.java
@@ -2,12 +2,15 @@ package pl.seahawks.vacationes.service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.seahawks.vacationes.converter.UserConverter;
+import pl.seahawks.vacationes.exception.UserAlreadyExistsException;
 import pl.seahawks.vacationes.repository.RoleRepository;
 import pl.seahawks.vacationes.repository.UserRepository;
 import pl.seahawks.vacationes.request.RegisterRequest;
+import pl.seahawks.vacationes.response.RegisterResponse;
 import pl.seahawks.vacationes.user.model.Role;
 import pl.seahawks.vacationes.user.model.User;
 
@@ -24,17 +27,29 @@ public class RegistrationService {
     private final UserConverter userConverter;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final UserService userService;
 
     private final Role DEFAULT_ROLE = Role.ROLE_USER;
 
     @Transactional
-    public Long registerNewUser(RegisterRequest request) {
+    public RegisterResponse registerNewUser(RegisterRequest request) {
+        if (userService.isUserExists(request)) {
+            return RegisterResponse
+                    .builder()
+                    .success(false)
+                    .exception(new UserAlreadyExistsException(request.getEmail()))
+                    .build();
+        }
         User user = userConverter.from(request);
         user.setUserRoles(new HashSet<>());
         updateCredentials(user);
         userRepository.save(user);
-        return user.getId();
+        return RegisterResponse.builder()
+                .success(true)
+                .id(user.getId())
+                .build();
     }
+
 
     private void updateCredentials(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
