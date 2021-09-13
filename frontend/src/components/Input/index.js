c215955ad4e3700398dde5dfa76d2229d2e18bec
Konrad Kukla <kondziocard@gmail.com>
2021-09-13 03:03:32 +0200

    create simple Input component

---
+++ frontend/src/components/Input/index.js
@@ -0,0 +1,24 @@
+import styles from './index.module.scss';
+
+const Input = ({
+  name = 'Email',
+  type = 'text',
+  placeholder = 'Twój adres email',
+}) => {
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor={name} className="label">
+        {name}
+      </label>
+      <input
+        name={name}
+        id={name}
+        type={type}
+        className="input"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default Input;
