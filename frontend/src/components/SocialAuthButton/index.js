37fbd8b1273891d68d2bfcf75963073a9628238a
Konrad Kukla <kondziocard@gmail.com>
2021-09-13 03:01:15 +0200

    create Button component to sign in using social media account

---
+++ frontend/src/components/SocialAuthButton/index.js
@@ -0,0 +1,12 @@
+import styles from './index.module.scss';
+
+const SocialAuthButton = ({ socialIcon, socialName, children }) => {
+  return (
+    <button className={styles.socialAuthButton}>
+      <img src={socialIcon} alt={`${socialName} logo`} />
+      <p>{children}</p>
+    </button>
+  );
+};
+
+export default SocialAuthButton;
