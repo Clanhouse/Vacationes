7ecff9fdce01b4068cba185c74c3bd8596d3c570
Konrad Kukla <kondziocard@gmail.com>
2021-09-23 15:46:45 +0200

    create 404 error page

---
+++ frontend/src/views/PageNotFound.js
@@ -0,0 +1,25 @@
+import styles from './PageNotFound.module.scss';
+import notFoundImage from 'assets/images/404.svg';
+import Heading from 'components/Heading';
+import Text from 'components/Text';
+import Button from 'components/Button';
+import { Link } from 'react-router-dom';
+
+const PageNotFound = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.heroImageWrapper}>
+        <img className={styles.heroImage} src={notFoundImage} alt="hero" />
+      </div>
+      <div className={styles.formWrapper}>
+        <Heading>Ups!</Heading>
+        <Text>Coś poszło nie tak</Text>
+        <Link to="/">
+          <Button>Powrót do strony głównej</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
