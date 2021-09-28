a1a4bd701225207fb9bb08c15c4200eca8b09892
Arkadiusz Konopka <arekcommerce@gmail.com>
2021-09-28 03:43:04 +0200

    create redux with toolkit

--- frontend/src/Redux/slices/favouriteSlice.js
+++ frontend/src/Redux/slices/favouriteSlice.js
@@ -1,18 +1,18 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value : []
-}
+  value: [],
+};
 
-export const favourite = createSlice({
+export const favouriteSlice = createSlice({
   name: 'favouriteList',
   initialState,
-  reducers:{
-    addItem: (state, action)=>{
-      state.value.push(action.payload) 
+  reducers: {
+    addItem: (state, action) => {
+      state.value.push(action.payload);
     },
-    removeItem=(state,payload)=>{
+    removeItem: (state, payload) => {},
+  },
+});
 
-    }
-  }
-})
+export default favouriteSlice.reducer;
