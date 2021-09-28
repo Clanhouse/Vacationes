c3da398e9e7933a89b0377a746efc06113594727
Arkadiusz Konopka <arekcommerce@gmail.com>
2021-09-28 03:43:04 +0200

    create redux with toolkit

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import favouriteReducer from './slices/favouriteSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favouriteReducer,
+  },
 });
