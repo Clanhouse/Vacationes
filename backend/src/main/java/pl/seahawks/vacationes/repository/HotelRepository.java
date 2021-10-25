6ab58720462fe0011293a650219fe4b9d8cdf6f0
sosadnik <32511347+sosadnik@users.noreply.github.com>
2021-10-25 12:31:39 +0200

    Revert "Vac 20"

--- backend/src/main/java/pl/seahawks/vacationes/repository/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.seahawks.vacationes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.seahawks.vacationes.domain.model.Hotel;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-}
--- backend/src/main/java/pl/seahawks/vacationes/user/model/Role.java
+++
@@ -1,5 +0,0 @@
-package pl.seahawks.vacationes.user.model;
-
-public enum Role {
-    ROLE_ADMIN, ROLE_USER, ROLE_MODERATOR;
-}
